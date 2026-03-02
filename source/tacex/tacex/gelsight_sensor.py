Fix: GelSight normalization of the camera_depth and height_map debug_images.
Instead of normalizing each image with min/max,
we normalize based on the gelpad dimension (= its height).

Also:
Convention for height map generation adjusted and add fixes for height_map
- prev.:
  - 0 values in the height_map equals no indentation
  - height_map values >0 meant, indentation inside gel
- now:
  - 0 values in height_map == highest point of gelpad
  - height_map values are <= 0, where the smaller, the closer to the camera
    -> this means, smallest value = biggest indentation
  - height_map is stricly now in range [-gelpad_height, 0]

This change does not really affect the Taxim Sim much.
Its mainly to make it easier to understand the height map definition.
-> You can imagine the height_map now as the gelpad surface when
its getting pressed towards the camera.
With the 0-reference point of the height map being at the highest point of the gelpad.

This change was also beneficial for the height_map normalization w.r.t. the gelpad dimension.

source/tacex/tacex/gelsight_sensor.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import numpy as np
 import torch
 from collections.abc import Sequence
-from matplotlib import pyplot as plt
 from typing import TYPE_CHECKING
 
 import cv2
@@ -469,10 +468,7 @@ class GelSightSensor(SensorBase):
                     # frame = np.moveaxis(frame, 0, -1)
                     # convert to 3 channel image, to later turn it into 4 channel RGBA for Isaac Widget
                     frame = np.dstack((frame, frame, frame)).astype(np.uint8)
-                    frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-
                     # update image of the window
-                    frame = frame.astype(np.uint8)
                     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)  # cv.COLOR_BGR2RGBA) COLOR_RGB2RGBA
                     height, width, channels = frame.shape
                     with self._windows["camera_depth"][str(i)].frame:
@@ -505,12 +501,19 @@ class GelSightSensor(SensorBase):
                         )  # default format omni.ui.TextureFormat.RGBA8_UNORM
 
                     frame = self._data.output["height_map"][i].cpu().numpy()
+                    # map [-gelpad_height, 0] to [0,255] for proper depth image of height_map
+                    frame = (
+                        (
+                            np.clip(frame, -self.cfg.gelpad_dimensions.height * 1000, 0)
+                            + self.cfg.gelpad_dimensions.height * 1000
+                        )
+                        / (self.cfg.gelpad_dimensions.height * 1000)
+                        * 255
+                    ).astype(np.uint8)
                     # convert to 3 channel image, to later turn it into 4 channel RGBA for Isaac Widget
                     frame = np.dstack((frame, frame, frame)).astype(np.uint8)
-                    frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
                     # update image of the window
-                    frame = frame.astype(np.uint8)
                     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)  # cv.COLOR_BGR2RGBA) COLOR_RGB2RGBA
                     height, width, channels = frame.shape
                     with self._windows["height_map"][str(i)].frame:
@@ -579,19 +582,19 @@ class GelSightSensor(SensorBase):
             # clip camera values that are = inf
             depth_output[torch.isinf(depth_output)] = self.cfg.sensor_camera_cfg.clipping_range[1]
 
+            # add a channel to the depth image for debug_vis
             self._data.output["camera_depth"] = depth_output.reshape(
-                (self._num_envs, 1, self.camera_resolution[1], self.camera_resolution[0])
-            )  # add a channel to the depth image for debug_vis
-            self._data.output["camera_depth"] *= 1000.0
-
-            # normalize the depth image
-            normalized = self._data.output["camera_depth"].view(self._data.output["camera_depth"].size(0), -1)
-            normalized -= self.cfg.sensor_camera_cfg.clipping_range[0] * 1000
-            normalized /= self.cfg.sensor_camera_cfg.clipping_range[1] * 1000
-            normalized = (normalized * 255).type(dtype=torch.uint8)
-            self._data.output["camera_depth"] = normalized.reshape(
                 (self._num_envs, self.camera_resolution[1], self.camera_resolution[0], 1)
-            )  # add a channel to the depth image for debug_vis
+            )
+
+            # normalize the depth image in clipping range
+            normalized = (self._data.output["camera_depth"] - self.cfg.sensor_camera_cfg.clipping_range[0]) / (
+                self.cfg.sensor_camera_cfg.clipping_range[1] - self.cfg.sensor_camera_cfg.clipping_range[0]
+            )
+            normalized = (normalized * 255).type(dtype=torch.uint8)
+            self._data.output["camera_depth"] = normalized
+
+            # self._data.output["camera_depth"] *= 1000.0
 
         return self._data.output["camera_depth"]
 
@@ -604,6 +607,15 @@ class GelSightSensor(SensorBase):
             self._data.output["height_map"][torch.isinf(self._data.output["height_map"])] = (
                 self.cfg.sensor_camera_cfg.clipping_range[1]
             )
+            # transform camera depth values to values in gelpad range, i.e. [0, gelpad_height]
+            self._data.output["height_map"] -= self.cfg.sensor_camera_cfg.clipping_range[0]
+            # height_map == 0 should mean that no indentation, min(height_map) == biggest indentation
+            self._data.output["height_map"] -= self.cfg.gelpad_dimensions.height
+
+            self._data.output["height_map"] = self._data.output["height_map"].clamp(
+                -self.cfg.gelpad_dimensions.height, 0
+            )
+
             # default unit is meter -> convert to mm for optical sim
             self._data.output["height_map"] *= 1000
 
@@ -613,21 +625,6 @@ class GelSightSensor(SensorBase):
             # e.g. use soft body deformation as height map? -> not implemented yet
             # or that we dont need a height map in general
             pass
-
-    # def _show_height_map_inside_gui(self, index):
-    #     plt.close()
-    #     height_map = self._data.output["height_map"][index].cpu().numpy()
-    #     np.save("height_map.npy", height_map)
-
-    #     X = np.arange(0, height_map.shape[0])
-    #     Y = np.arange(0, height_map.shape[1])
-    #     X, Y = np.meshgrid(X, Y)
-    #     Z = height_map
-    #     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    #     ax.plot_surface(X, Y, Z.T)
-    #     # plt.show()
-    #     print("saving height_map img")
-    #     plt.savefig(f"height_map{index}.png")
 
     def _create_tiled_camera(self) -> TiledCamera:
         # compute camera parameters for desired field-of-view

source/tacex/tacex/simulation_approaches/gpu_taxim/taxim_sim.py
@@ -107,16 +107,18 @@ class TaximSimulator(GelSightSimulator):
         return self.tactile_rgb_img
 
     def compute_indentation_depth(self):
-        height_map = self.sensor._data.output["height_map"] / 1000  # convert height map from mm to meter
-        min_distance_obj = height_map.amin((1, 2))
-        # smallest distance between object and sensor case
-        dist_obj_sensor_case = min_distance_obj - self.cfg.gelpad_to_camera_min_distance
-        dist_obj_sensor_case = torch.where(dist_obj_sensor_case < 0, 0, dist_obj_sensor_case)
+        height_map = self.sensor._data.output["height_map"]
+        # for old height map
+        # min_distance_obj = height_map.amin((1, 2))
+        # # smallest distance between object and sensor case
+        # dist_obj_sensor_case = min_distance_obj - self.cfg.gelpad_to_camera_min_distance * 1000
+        # dist_obj_sensor_case = torch.where(dist_obj_sensor_case < 0, 0, dist_obj_sensor_case)
 
-        self._indentation_depth[:] = torch.where(
-            dist_obj_sensor_case <= self.cfg.gelpad_height, (self.cfg.gelpad_height - dist_obj_sensor_case) * 1000, 0
-        )
+        # self._indentation_depth[:] = torch.where(
+        #     dist_obj_sensor_case <= self.cfg.gelpad_height, (self.cfg.gelpad_height * 1000 - dist_obj_sensor_case), 0
+        # )
 
+        self._indentation_depth[:] = -height_map.amin((1, 2))
         return self._indentation_depth
 
     def reset(self):